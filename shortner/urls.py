080015775d33ff9889eb84e6a9945578c6456281
Omer Faruk Coskun <ofcskn.omer@gmail.com>
2022-09-15 23:21:29 +0300

    Successful page

--- shortner/urls.py
+++ shortner/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'shortner'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:url_short_code>', views.go, name='go'),
     path('do', views.do, name='do'),
+    path('yuppi/<str:url_short_code>', views.successfully_created, name='yuppi'),
 ]
--- shortner/views.py
+++ shortner/views.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
 import uuid
-from django.shortcuts import render
 
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render,redirect, get_object_or_404
 from shortner.models import Url
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+from django.template import loader
+import socket
 
 def index(request):
     return render(request, 'index.html')
@@ -11,7 +16,28 @@ def index(request):
 def do(request):
     if request.method == 'POST':
         url = request.POST['long_url']
-        url_short_code = str(uuid.uuid4())[:6]
-        new_url = Url(long_url = url, url_short_id = url_short_code)
-        new_url.save()
-        return HttpResponse(url_short_code)
+        val = URLValidator()
+        try:
+            # validate the url
+            val(url)
+            # create url short id
+            url_short_code = str(uuid.uuid4())[:6]
+            # create new url class 
+            new_url = Url(long_url = url, url_short_id = url_short_code)
+            # save new url to the database
+            new_url.save()
+            return HttpResponseRedirect(reverse('shortner:yuppi', args=(url_short_code)))
+        except ValidationError:
+            return HttpResponse('Sorry, this is not a valid url.')
+
+def successfully_created(request, url_short_code):
+    print('url_short_code', url_short_code)
+    # get url by url_short_code
+    url_from_database = get_object_or_404(Url, url_short_id = url_short_code)
+    context = {
+        'url': url_from_database,
+    }
+    return render(request, 'successful.html', context)
+
+def go(request, url_short_code):
+    return HttpResponse('go to ' + url_short_code)
